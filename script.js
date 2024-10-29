c89d544ea154ad2a735e1bda5af691ad3160435e
Sandusha20049887 <sandusha1997@gmail.com>
2024-10-29 15:26:32 +0000

    ajax call to retrieve posts

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 
 $(document).ready(function() {
+
+    $.ajax({
+        url: 'http://localhost:8080/getPost', // Replace with your API URL
+        type: 'GET',
+        success: function(data) {
+            appendPosts(data);
+        },
+        error: function(error) {
+            console.error('Error:', error);
+        }
+    });
+
+    
     $('#addPost').click(function(e){
         e.preventDefault();
         alert("clicked");
     });
-
-    
 });
+
+function appendPosts(posts) {
+    // Clear existing posts
+    $('#post').empty();
+    //loop thorugh the records
+    posts.forEach(post => {
+        console.log(post.make);
+    });
+}
