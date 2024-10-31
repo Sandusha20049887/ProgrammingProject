57d65d94520977986005c9580db023d44d18fc4f
Sandusha20049887 <sandusha1997@gmail.com>
2024-10-31 03:28:26 +0000

    login

--- login.js
+++ login.js
@@ -1,8 +1,24 @@
 $(document).ready(function () {
 
     $('#login').click(function () {
-        sessionStorage.removeItem("userid");
-        $(this).redirect("/login.html");
+
+        const credentials = {
+            email: $('#email').val(),
+            password: $('#password').val()
+        };
+
+        $.ajax({
+            url: 'http://localhost:8000/login',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(credentials),
+            success: function (resp) {
+                console.log(resp);
+            },
+            error: function (error) {
+                console.log('Error:', error.responseText);
+            }
+        });
     })
 
     //sessionStorage.setItem("userid", "123");
