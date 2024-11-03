21916f75582bc2ea23cc9a53c9df114b76c8b893
Sandusha20049887 <sandusha1997@gmail.com>
2024-11-03 09:14:40 +0000

    add update post

--- post.js
+++ post.js
@@ -1,3 +1,34 @@
 $(document).read(function(){
+    $('#addPost').click(function (e) {
+        e.preventDefault();
+        const today = new Date();
 
+        const postDetails = {
+            make: $('#make').val(),
+            model: $('#model').val(),
+            year: $('#year').val(),
+            faultDescription: $('#faultDescription').val(),
+            garageName: $('#garageName').val(),
+            garageAddress: $('#garageAddress').val(),
+            contactNo: $('#contactNo').val(),
+            status: $('#status').val(),
+            userId: userId,
+            datePosted: today.toISOString().split('T')[0]
+        };
+        console.log(postDetails);
+        $.ajax({
+            url: 'http://localhost:8000/addPost',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(postDetails),
+            success: function (resp) {
+                alert(resp);
+                getUserPosts(userId);
+            },
+            error: function (error) {
+                console.log('Error:', error);
+                alert('Err - Record not added !');
+            }
+        });
+    });
 });
