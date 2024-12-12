dd0575523c83facb69c1adbb90e5a1fb5cd937b3
Sandusha20049887 <sandusha1997@gmail.com>
2024-12-12 01:54:53 +0000

    completed create testing

--- cypress/e2e/CRUD Testing/CreateSpec.cy.js
+++ cypress/e2e/CRUD Testing/CreateSpec.cy.js
@@ -10,6 +10,29 @@ describe('Create Post Flow', () => {
         cy.url().should('include', '/index');
         cy.get('#newPost').click();
 
-      
+        // Fill details and Create a Post
+        cy.url().should('include', '/addUpdatePost');
+        cy.get('#make').type('Toyota'); 
+        cy.get('#model').type('Camry'); 
+        cy.get('#year').type('2020'); 
+        cy.get('#faultDescription').type('Engine not starting'); 
+        cy.get('#garageName').type('Super Garage'); 
+        cy.get('#garageAddress').type('1234 Elm St'); 
+        cy.get('#contactNo').type('5855234'); 
+        cy.get('#status').select('progressing');
+
+        cy.on('window:alert', (str) => { 
+             expect(str).to.equal('Post added!'); 
+        });
+
+        cy.get('#addPost').click();
+
+        // Verify created post
+        cy.url().should('include', '/index');
+        cy.get('.post-box').last().within(() => {
+            cy.get('h5').should('contain', 'Toyota Camry');      
+            cy.get('.status').should('contain', 'progressing');     
+            cy.contains('Year of manufacture : 2020');            
+        });
     });
 });
