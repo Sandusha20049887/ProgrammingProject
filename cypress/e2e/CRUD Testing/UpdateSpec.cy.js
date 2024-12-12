a3ba20d75af5749e642b5f03bb8114968ae0cf09
Sandusha20049887 <sandusha1997@gmail.com>
2024-12-12 03:37:06 +0000

    Completed testing for update

--- cypress/e2e/CRUD Testing/UpdateSpec.cy.js
+++ cypress/e2e/CRUD Testing/UpdateSpec.cy.js
@@ -1,5 +1,5 @@
-describe('Create Post Flow', () => {
-    it('should log in, navigate to create page, and create a post', () => {
+describe('Update Post Flow', () => {
+    it('should log in, navigate to user post page, and navigate to update post', () => {
         // Log in to the application
         cy.visit('https://programming-project-nu.vercel.app/login.html');
         cy.get('#email').type('test@dbs.ie');
@@ -9,5 +9,36 @@ describe('Create Post Flow', () => {
         // Go to user post page
         cy.url().should('include', '/index');
         cy.get('#userPost').click();
+
+        // Go to update page (select top 1 record)
+        cy.url().should('include', '/userPost');
+
+        cy.get('.list-group-item').first().within(() => { 
+            cy.get('.updatePost').click();
+        });
+
+        // Fill in only specific fields in Update Form 
+        cy.url().should('include', '/addUpdatePost'); 
+        cy.get('#ufaultDescription').clear().type('Battery issue'); 
+        cy.get('#ugarageName').clear().type('Quality Garage'); 
+        cy.get('#ugarageAddress').clear().type('7890 Maple St'); 
+        cy.get('#ucontactNo').clear().type('555006789'); 
+        cy.get('#ustatus').select('resolved');
+
+        // verify alert text
+        cy.on('window:alert', (str) => { 
+            expect(str).to.equal('Post Updated!'); 
+       });
+
+       cy.get('#updatePost').click();
+
+        // Verify updated post
+        cy.url().should('include', '/userPost');
+        cy.get('.list-group-item').first().within(() => {
+            cy.contains('Fault Description : Battery issue'); 
+            cy.contains('Garage Name : Quality Garage'); 
+            cy.contains('Address : 7890 Maple St'); 
+            cy.contains('Contact No : 555006789');           
+        });
     })
 });
